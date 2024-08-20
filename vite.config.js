3a4951515b2a96b3f995e253cca59fefd00593e3
RAKAN ASFOUR <rakanasfour@RAKANs-MacBook-Air.local>
2024-08-20 11:39:57 -0700

    change the uri from default to backend

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/': {
-        target: 'http://mongo-backend-service.default.svc.cluster.local',
+        target: 'http://mongo-backend-service.backend.svc.cluster.local',
         changeOrigin: true,
       },
     },
